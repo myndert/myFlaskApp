0cec2efb41cb39c9847e07ce791909a6393c493f
Ricky Li <ricky_0321@hotmail.com>
2018-08-01 22:07:27 -0500

    my first flask app

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+  articles = [
+    {
+      'id': 1,
+      'title' : 'Article One',
+      'body' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quaerat aspernatur laborum nulla, animi aliquid! Quisquam illum fugit explicabo, cum dicta vero atque harum perferendis nulla optio sed debitis maxime.',
+      'author' : 'Brad',
+      'create_date' : '07-01-2018'
+    },
+        {
+      'id': 2,
+      'title' : 'Article Two',
+      'body' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quaerat aspernatur laborum nulla, animi aliquid! Quisquam illum fugit explicabo, cum dicta vero atque harum perferendis nulla optio sed debitis maxime.',
+      'author' : 'John',
+      'create_date' : '07-01-2018'
+    },
+        {
+      'id': 3,
+      'title' : 'Article Three',
+      'body' : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quaerat aspernatur laborum nulla, animi aliquid! Quisquam illum fugit explicabo, cum dicta vero atque harum perferendis nulla optio sed debitis maxime.',
+      'author' : 'David',
+      'create_date' : '07-01-2018'
+    }
+  ]
+  return articles
